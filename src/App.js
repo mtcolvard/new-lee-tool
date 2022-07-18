8e248c3c02c26c4520e8dfc5bd5b580b0126084b
mtcolvard <matthew.colvard@gmail.com>
2022-07-18 19:46:31 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Lee Brenner's Noise Simulation Tool
         </a>
       </header>
     </div>
